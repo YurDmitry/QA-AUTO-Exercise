aac8e3f314581648e03cbf5f4a7bcbfcf949f08b
Dmitry Yurchenko <dzmitry_yurchanka@senla.eu>
2022-12-20 17:01:12 +0300

    Exercise 4

--- Exercise_4/src/Main.java
+++ Exercise_4/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        System.out.println("Exercise 4:");
+        int A = 200, sell = 15, B = 30;
+        int sum = 10*200-10*A/100*15 + 2*B;
+        System.out.println("Итоговая цена: \n" + sum);
     }
 }
